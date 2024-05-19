87cbf37088d03ce421a7fdf481ea68e97ffe9145
Md-Shajib <72320464+Md-Shajib@users.noreply.github.com>
2024-05-19 14:27:48 +0600

    First Commit
    cse fest 2022

---
+++ media/js/carousel-customize.js
@@ -0,0 +1,34 @@
+$('#gallery').owlCarousel({
+      loop:true,
+      margin: 5,
+      dots:true ,
+      autoplay:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          775:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+})
+$('#support').owlCarousel({
+      loop:true,
+      margin:5,
+      dots:true ,
+      autoplay:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          775:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+})
